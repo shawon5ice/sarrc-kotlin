37472d483227aa225fdd85bb1740908e49415b58
shawon5ice <shawon5ice@gmail.com>
2020-11-23 23:13:06 +0600

    default ArrayAdaptor

--- app/src/main/java/com/ssquare/sarrccountries/MainActivity.kt
+++ app/src/main/java/com/ssquare/sarrccountries/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.ssquare.sarrccountries
 
+import android.content.res.Resources
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import android.widget.TextView
+import android.widget.Toast
+import java.time.temporal.Temporal
+import java.util.*
+
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val res: Resources = resources
         setContentView(R.layout.activity_main)
+
+        val countryNames = res.getStringArray(R.array.country_name)
+        var listView = findViewById<ListView>(R.id.listViewId)
+        val adaptor = ArrayAdapter(this,android.R.layout.simple_list_item_1,countryNames)
+        listView.adapter = adaptor
     }
 }
